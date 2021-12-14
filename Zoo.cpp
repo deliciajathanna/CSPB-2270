2d10194cd70d1913d524d64254c83a4ddf6731af
deliciajathanna <93067206+deliciajathanna@users.noreply.github.com>
2021-12-14 15:20:04 -0700

    Update Zoo.cpp

--- Zoo.cpp
+++ Zoo.cpp
@@ -1,5 +1,6 @@
-#include "Zoo.h"
+#include "FinalProject.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 Zoo::Zoo() {
@@ -9,27 +10,114 @@ Zoo::~Zoo() {
 }
 
 void Zoo::EnterAnimalType(int type) {
+    string answer;
+    string name;
+    int age;
+    bool loop = true;
     if (type == 1) {
-        // this will help end the while loop
-        bool loop;
-        string answer;
-        string name;
-        int age;
-        cout << "Enter the name and age of the Lion:  " << endl;
+        cout << "Enter the name of the Lion:  " << endl;
         cin >> name;
+        cout << "Enter the age of the Lion: " << endl;
         cin >> age;
-        InsertLion(name, age);
-        // while loop to ensure user is entering the corrent number of lions
+        InsertAnimal(name, age);
         while (loop){
-            cout << "Do you want to enter another Lion?" << endl;
+            cout << "Do you want to enter another Lion? Enter Y or N: " << endl;
             cin >> answer;
             if (answer == "Y") {
-            cout << "Enter the name and age of the Lion:  " << endl;
-            cin >> name;
-            cin >> age;
-            InsertLion(name, age);
+                cout << "Enter the name of the Lion:  " << endl;
+                cin >> name;
+                cout << "Enter the age of the Lion: " << endl;
+                cin >> age;
+                InsertAnimal(name, age);
             }
-            // if no the loop ends
+            if (answer == "N") {
+                cout << "Taking you back to the main menu"<< endl;
+                loop = false;
+            }
+        }
+    }
+    if (type == 2) {
+        cout << "Enter the name of the Tiger:  " << endl;
+        cin >> name;
+        cout << "Enter the age of the Tiger: " << endl;
+        cin >> age;
+        InsertAnimal(name, age);
+        while (loop){
+            cout << "Do you want to enter another Tiger? Enter Y or N: " << endl;
+            cin >> answer;
+            if (answer == "Y") {
+                cout << "Enter the name of the Tiger:  " << endl;
+                cin >> name;
+                cout << "Enter the age of the Tiger: " << endl;
+                cin >> age;
+                InsertAnimal(name, age);
+            }
+            if (answer == "N") {
+                cout << "Taking you back to the main menu"<< endl;
+                loop = false;
+            }
+        }
+    }
+    if (type == 3) {
+        cout << "Enter the name of the Bear:  " << endl;
+        cin >> name;
+        cout << "Enter the age of the Bear: " << endl;
+        cin >> age;
+        InsertAnimal(name, age);
+        while (loop){
+            cout << "Do you want to enter another Bear? Enter Y or N: " << endl;
+            cin >> answer;
+            if (answer == "Y") {
+                cout << "Enter the name of the Bear:  " << endl;
+                cin >> name;
+                cout << "Enter the age of the Bear: " << endl;
+                cin >> age;
+                InsertAnimal(name, age);
+            }
+            else if (answer == "N") {
+                cout << "Taking you back to the main menu"<< endl;
+                loop = false;
+            }
+        }
+    }
+    if (type == 4) {
+        cout << "Enter the name of the Bird:  " << endl;
+        cin >> name;
+        cout << "Enter the age of the Bird: " << endl;
+        cin >> age;
+        InsertAnimal(name, age);
+        while (loop){
+            cout << "Do you want to enter another Bird? Enter Y or N: " << endl;
+            cin >> answer;
+            if (answer == "Y") {
+                cout << "Enter the name of the Bird:  " << endl;
+                cin >> name;
+                cout << "Enter the age of the Bird: " << endl;
+                cin >> age;
+                InsertAnimal(name, age);
+            }
+            else if (answer == "N") {
+                cout << "Taking you back to the main menu"<< endl;
+                loop = false;
+            }
+        }
+    }
+    if (type == 5) {
+        cout << "Enter the name of the Elephant:  " << endl;
+        cin >> name;
+        cout << "Enter the age of the Elephant" << endl;
+        cin >> age;
+        InsertAnimal(name, age); 
+        while (loop) {
+            cout << "Do you want to enter another Elephant? Enter Y or N: " << endl;
+            cin >> answer;
+            if (answer == "Y") {
+                cout << "Enter the name of the Elephant: " << endl;
+                cin >> name;
+                cout << "Enter the age of the Elephant: " << endl;
+                cin >> age;
+                InsertAnimal(name, age);
+            }
             if (answer == "N") {
                 cout << "Taking you back to the main menu"<< endl;
                 loop = false;
@@ -37,18 +125,53 @@ void Zoo::EnterAnimalType(int type) {
         }
     }
 }
-void Zoo::InsertLion(string name, int age){
-    // insert the lion(s) into the lion linked list
-    shared_ptr<Animal> lion(new Animal);
-    lion->name = name;
-    lion->age = age;
-    lion->next = NULL;
+
+void Zoo::InsertAnimal(string name, int age){
+    shared_ptr<Animal> list(new Animal);
+    list->name = name;
+    list->age = age;
+    list->next = NULL;
     if(head == NULL) {
-        head = lion;
-        tail = lion;
+        head = list;
+        tail = list;
     }
-    else{
-        tail->next = lion;
+    else {
+        tail->next = list;
         tail = tail->next;
+    }
+}
+  
+
+void Zoo::RemoveAnimal(string name) {
+    shared_ptr<Animal> head = GetTop();
+    if (head == NULL) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    while(head != NULL) {
+        if (head->name == name) {
+            head->name = head->next->name;
+            head->age = head->next->age;
+
+            cout << name << " was deleted from the list." << endl;
+            return;
         }
+        else {
+            head = head->next;
+        }
+    }
+}
+    
+    
+    
+
+shared_ptr<Animal> Zoo::GetTop(){
+  return head;
+}
+
+void Zoo::DisplayAnimal(shared_ptr<Animal> head) {
+   if(head != NULL) {
+       cout << head->name << "     " << head->age<< endl;   
+       DisplayAnimal(head->next);
+   }
 }
